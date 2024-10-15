6a874e223c01d08b1919b5953a420e219fb9d777
tgu <stevengu2019@hotmail.com>
2024-10-15 13:42:53 +0200

    liveness check

--- ImpOpt/liveness.ml
+++ ImpOpt/liveness.ml
@@ -2,12 +2,15 @@ open Imp
 open Nimp
 
 module VSet = Set.Make(String)
+module VMap = Map.Make(String)
 
 (* returns the set of variables accessed by the expression [e] *)
 let rec use_expr e = match e with
-  | Cst _ -> VSet.empty
-  | Var x -> VSet.singleton x
-  | _ -> failwith "not implemented"
+  | Cst _  -> VSet.empty
+  | Bool _ -> VSet.empty
+  | Var x  -> VSet.singleton x
+  | Binop (op, e1, e2) -> VSet.union (use_expr e1) (use_expr e2)
+  | Call(_,args) -> List.fold_left (VSet.union) VSet.empty (List.map use_expr args)
 
 let liveness fdef =
   let n = max_instr_list fdef.code in
@@ -17,11 +20,37 @@ let liveness fdef =
      exit of [i] *)
   let rec lv_in_instr i lv_out = match i.instr with
     (* by case on the contents of i.instr *)
-    | Putchar(e) -> let s = VSet.union lv_out (use_expr e) in
-                    live.(i.nb) <- s;
-                    s
+    | Putchar(e)  -> let s = VSet.union lv_out (use_expr e) in
+                     live.(i.nb) <- s;
+                     s
+    | Set(x,e)    -> let s  = VSet.union lv_out (use_expr e) in
+                     let s' = VSet.diff s (VSet.singleton x) in
+                     live.(i.nb) <- s';
+                     s'
+    | If(e,seq1,seq2) -> 
+        let s1 = lv_in_list seq1 lv_out in
+        let s2 = lv_in_list seq2 lv_out in
+        let s = s1 |> VSet.union s2 |> VSet.union (use_expr e) in
+        live.(i.nb) <- s;
+        s
+    
     (* While ? two rounds are enough *)
-    | _ ->failwith "not implemented"
+    | While(e, seq) ->
+        let lv_out' = lv_in_list seq lv_out |> VSet.union (use_expr e) in
+        let s = lv_in_list seq lv_out' in
+        let s' = s |> VSet.union (use_expr e) in
+        live.(i.nb) <- s';
+        s'
+    | Expr(e)  -> let s = VSet.union lv_out (use_expr e) in
+        live.(i.nb) <- s;
+        s
+    
+    | Return(e)  -> let s = VSet.union lv_out (use_expr e) in
+        live.(i.nb) <- s;
+        s
+                                    
+
+
   (* the same for a sequence, and records in [live] the live sets computed
      on entry to each analyzed instruction *)
   and lv_in_list l lv_out = match l with
@@ -37,4 +66,18 @@ let liveness_intervals_from_liveness fdef =
   let live = liveness fdef in
   (* for each variable [x], create the smallest interval that contains all
      the numbers of instructions where [x] is live *)
-  failwith "not implemented"
+  let intervals = ref VMap.empty in
+  let add_interval i var intervals =
+    try
+      let (start, finish) = VMap.find var intervals in
+      VMap.add var (min start i, max finish i) intervals
+    with Not_found ->
+      VMap.add var (i, i) intervals
+    in
+  for i = 0 to Array.length live - 1 do
+    VSet.iter (fun var ->
+      intervals := add_interval i var !intervals
+    ) live.(i)
+  done;
+    VMap.bindings !intervals
+
